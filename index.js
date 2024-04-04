a40f877b7deeb88c2a03472856afae4163d7d60c
WesleySid <160278189+WesleySid@users.noreply.github.com>
2024-04-04 20:40:57 +0200

    second commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(
-      "https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=FHmiZnYEhOqgaFA8"
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=FHmiZnYEhOqgaFA8`
     );
 
     return res.status(200).json(response.data);
@@ -44,9 +44,49 @@ app.get("/characters", async (req, res) => {
 app.get("/character/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    console.log("Fetching character with ID:", id);
+    console.log(id);
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/character/${id}?apiKey=FHmiZnYEhOqgaFA8`
+    );
+
+    const character = response.data;
+    console.log(character);
+    res.json(character);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const name = req.query.name || "";
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
+
+    console.log("Name:", name);
+    console.log("Skip:", skip);
+    console.log("Limit:", limit);
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=FHmiZnYEhOqgaFA8&skip=${skip}`
+    );
+
+    console.log(response.data);
+
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/comic/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comic/${id}?apiKey=FHmiZnYEhOqgaFA8`
     );
 
     const character = response.data;
